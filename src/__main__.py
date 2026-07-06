a9aca76487248f02d1669b9ae0907b9e09637472
Asaph Nzongan <anzongan@mi-c4-p2.s19.be>
2026-07-06 17:25:50 +0200

    feat: prompt builder defined

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,42 @@
 import argparse
+from pathlib import Path
+from json import loads, JSONDecodeError
+
+FORMAT_RESPONSE ="""
+{
+    "prompt": "[prompt]",
+    "name": "[function_name]",
+    "parameters": [parameters]
+}
+"""
+
+def get_str_prompts(file_path: str) -> list[str]:
+    path = Path(file_path)
+    try:
+        text = path.read_text()
+        dict_prompts = loads(text)
+    except (OSError, JSONDecodeError)  as e:
+        raise ValueError(f"impossible de charger les donnees depuis {file_path}") from e
+    else:
+        return [str(rup) for rup in dict_prompts]
+
+def get_func_defs(file_path):
+    path = Path(file_path)
+    try:
+        return path.read_text()
+    except OSError as e:
+        raise ValueError(f"impossible de charger les donnees depuis {file_path}") from e
+
+
+def build_prompts(args: argparse.Namespace) -> list[str]:
+    user_prompts = get_str_prompts(args.input)
+    func_defs = get_func_defs(args.functions_definitions)
+    prompts = []
+    for up in user_prompts:
+        prompt = "\n".join((up, func_defs, FORMAT_RESPONSE))
+        prompts.append(prompt)
+    return prompts
+    
 
 def main():
     parser = argparse.ArgumentParser()
--- src/parse_input.py
+++ src/parse_input.py
@@ -14,6 +14,7 @@ class FunctionDef(BaseModel):
     parameters: dict[str, VarMetaData]
     returns: VarMetaData
 
+
 def load_function_from_json(file_path: str) -> list[dict[str, str | dict]]:
     path = Path(file_path)
     try:
@@ -26,5 +27,42 @@ def load_function_from_json(file_path: str) -> list[dict[str, str | dict]]:
         raise ValueError(
                 f"Impossible de charger les donnees depuis {file_path}: {e}"
             ) from e
-    
+
 print(load_function_from_json("data/input/functions_definition.json"))
+
+"""
+function definition is inserted in the prompt as text
+
+[Prompt Utilisateur + Définition Fonctions + format_response] 
+                  │
+                  ▼
+          1. sdk.encode()
+                  │
+                  ▼
+         [final_prompt_ids] ◄──────────────────────────────────────┐
+                  │                                                │
+                  ▼                                                │ (Boucle)
+       2. sdk.get_logits_from_input_ids()                          │
+                  │                                                │
+                  ▼                                                │
+           [vocab_logits]                                          │
+                  │                                                │
+                  ▼                                                │
+       3. TON CODE : Application du Masque                         │
+   (Force à -∞ les tokens JSON non valides)                        │
+                  │                                                │
+                  ▼                                                │
+       4. TON CODE : Échantillonnage (Sampling)                    │
+        (Sélectionne le meilleur token)                            │
+                  │                                                │
+                  ▼                                                │
+           [prochain_token_id] ────────────────────────────────────┘
+                  │
+                  ▼ (Quand le JSON est fermé : "}")
+           5. sdk.decode()
+                  │
+                  ▼
+         [Fichier JSON Final]
+
+À chaque tour de boucle, le prochain_token_id choisi à l'étape 4 est ajouté (append) à la liste final_prompt_ids.
+"""
